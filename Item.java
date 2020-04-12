c60eaae5cead3f6e89919dd3b646c545265499ba
88kirilN <62354146+88kirilN@users.noreply.github.com>
2020-04-12 20:48:58 +0200

    Add files via upload

--- Item.java
+++ Item.java
@@ -1,15 +1,53 @@
 class Item {
 	int id;
-	String name;
+	String ime;
 	double price;
 
 	//TODO add variable.
+	Character Tip;
 
 	//TODO constructor
+	Item(int id, String ime, double cena, Character Tip){
+		this.id = id;
+		this.ime = ime;
+		this.cena = cena;
+		this.Tip = Tip;
+	}
 
 	//TODO setters and getters
-
-	double taxReturn () {
+	public void setId(int newId){
+		this.id = newId;
+	}
+	public void setName(String newName){
+		this.ime = newIme;
+	}
+	public void setPrice(double newPrice){
+		this.cena = newCena;
+	}
+	public void setTaxType(Character newTaxType){
+		this.Tip = newTip;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	public String getIme(){
+		return ime;
+	}
+	public double getCena(){
+		return cena;
+	}
+	public Character getTip(){
+		return Tip;
+	}
+	
+	double taxReturn () { 
 		//TODO
+		if(Tip == 'A')
+			return cena * 0.18 * 0.15;
+		else if(Tip == 'B')
+			return cena * 0.05 * 0.15;
+		else
+			return 0;
 	}
 }
---
+++ Receipt.java
@@ -0,0 +1,10 @@
+class Receipt{
+	List<Item> niza = new List<Item>
+	
+	public addItem(Item A){
+		niza.addItem(A);
+	}
+	public deleteItem(Item A){
+		niza.deleteItem(A);
+	}
+}
